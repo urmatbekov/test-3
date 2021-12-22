051e6245f754416ab49fa422426ba8297a081727
Your Name <you@example.com>
2021-12-22 19:57:37 +0600

    1 test

---
+++ module.py
@@ -0,0 +1,4 @@
+class Cat(object):
+    def __init__(self, name):
+        self.name = name
+
---
+++ project/prg1.py
@@ -0,0 +1,12 @@
+import inspect
+import os
+import sys
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from module import Cat
+
+cat = Cat('Kiss Kiss')
+print(cat.name)
